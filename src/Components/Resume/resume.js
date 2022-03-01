e3e75c3b1a359b3155bcb17f61b033302afc2f64
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-03-01 13:12:37 -0600

    fixed resume image and link

--- src/Components/Resume/resume.js
+++ src/Components/Resume/resume.js
@@ -1,10 +1,13 @@
 import React from 'react'
+const resumePDF = require('./images/resumePDF.png')
 
 function resume() {
   return (
-    <div>
+    <div id="resume">
       <h2>Resume</h2>
-      <div id="resume"></div>
+      <div>
+        <a  href="https://docs.google.com/document/d/1_DyaDZphOEJHVtZxtbkl13LrWUGCMBTLMiyJ71ILP1M/edit?usp=sharing" target='blank'><img src={ resumePDF } alt="" /></a>
+      </div>
     </div>
   )
 }
